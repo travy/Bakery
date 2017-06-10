471c1eef04389c6220101ec71805bb5b9ae88a0a
Travis Anthony Torres <travis@travistorres.com>
2017-06-10 14:42:36 -0400

    tests:  Skeleton
    Created a test skeleton class for the Dish model.

---
+++ app/src/test/java/com/travistorres/bakery/models/DishTest.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2017 Travis Anthony Torres
+ */
+
+package com.travistorres.bakery.models;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * DishTest
+ *
+ * Test all methods are implemented correctly in the Dish data model.
+ *
+ * @author Travis Anthony Torres
+ * @version June 10, 2017
+ */
+
+public class DishTest {
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testInstantiatedObjectValid() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks error checking throws an exception on faulty input.
+     *
+     */
+    @Test
+    public void testInstantiatedObjectThrowsExceptionWhenServingsInvalid() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testToStringReturnsSameValueAsGetName() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testGetIdRetrievesTheMovieId() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testGetNameRetrievesTheCorrectName() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testSetNameSpecifiesTheCorrectName() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testGetServingsWorksAsExpected() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks the error checking system throws an Exception on faulty input.
+     *
+     */
+    @Test
+    public void testSetServingsSetsThrowsExceptionForSmallServings() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testSetServingsSetsServingsSize() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testGetImageRetrievesTheImageValue() {
+        Assert.fail("Not implemented");
+    }
+
+    /**
+     * Checks functionality works.
+     *
+     */
+    @Test
+    public void testSetImageChangesImageValue() {
+        Assert.fail("Not implemented");
+    }
+}
