6a5270ae9bc9bfd54b628c1fcc00754e0f6f9ac4
Travis Anthony Torres <travis@travistorres.com>
2017-06-10 15:32:04 -0400

    tests:  Dish
    Tested all methods implemented for the Dish data model.

--- app/src/test/java/com/travistorres/bakery/models/DishTest.java
+++ app/src/test/java/com/travistorres/bakery/models/DishTest.java
@@ -6,6 +6,8 @@ package com.travistorres.bakery.models;
 
 import junit.framework.Assert;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -18,22 +20,57 @@ import org.junit.Test;
  */
 
 public class DishTest {
+    private static final int DEFAULT_SHARED_DISH_ID = 1;
+    private static final String DEFAULT_SHARED_DISH_NAME = "Cookie";
+    private static final int DEFAULT_SHARED_DISH_SERVINGS = 6;
+    private static final String DEFAULT_SHARED_DISH_IMAGE = null;
+
+    private Dish dish;
+
+    /**
+     * Creates a dish object which can be used by all testing facilities.
+     *
+     */
+    @Before
+    public void setUp() {
+        dish = new Dish(
+                DEFAULT_SHARED_DISH_ID,
+                DEFAULT_SHARED_DISH_NAME,
+                DEFAULT_SHARED_DISH_SERVINGS,
+                DEFAULT_SHARED_DISH_IMAGE);
+    }
+
+    /**
+     * Releases any globally shared memory objects.
+     *
+     */
+    @After
+    public void tearDown() {
+        dish = null;
+    }
+
     /**
      * Checks functionality works.
      *
      */
-    @Test
-    public void testInstantiatedObjectValid() {
-        Assert.fail("Not implemented");
+    @Test(expected = IllegalArgumentException.class)
+    public void testInstantiatedObjectFailsOnZeroServings() {
+        Dish newDish = new Dish(
+                DEFAULT_SHARED_DISH_ID + 3,
+                DEFAULT_SHARED_DISH_NAME + "Atomic",
+                0,
+                "Some Image Resource"
+        );
     }
 
-    /**
-     * Checks error checking throws an exception on faulty input.
-     *
-     */
-    @Test
-    public void testInstantiatedObjectThrowsExceptionWhenServingsInvalid() {
-        Assert.fail("Not implemented");
+    @Test(expected = IllegalArgumentException.class)
+    public void testInstantiatedObjectFailsOnNegativeServings() {
+        Dish newDish = new Dish(
+                DEFAULT_SHARED_DISH_ID + 3,
+                DEFAULT_SHARED_DISH_NAME + "Atomic",
+                -2,
+                "Some Image Resource"
+        );
     }
 
     /**
@@ -42,7 +79,7 @@ public class DishTest {
      */
     @Test
     public void testToStringReturnsSameValueAsGetName() {
-        Assert.fail("Not implemented");
+        Assert.assertEquals(dish.getName(), dish.toString());
     }
 
     /**
@@ -51,7 +88,7 @@ public class DishTest {
      */
     @Test
     public void testGetIdRetrievesTheMovieId() {
-        Assert.fail("Not implemented");
+        Assert.assertEquals(DEFAULT_SHARED_DISH_ID, dish.getId());
     }
 
     /**
@@ -60,7 +97,7 @@ public class DishTest {
      */
     @Test
     public void testGetNameRetrievesTheCorrectName() {
-        Assert.fail("Not implemented");
+        Assert.assertEquals(DEFAULT_SHARED_DISH_NAME, dish.getName());
     }
 
     /**
@@ -69,7 +106,9 @@ public class DishTest {
      */
     @Test
     public void testSetNameSpecifiesTheCorrectName() {
-        Assert.fail("Not implemented");
+        String newName = "Snickerdoodle";
+        dish.setName(newName);
+        Assert.assertEquals(newName, dish.getName());
     }
 
     /**
@@ -78,16 +117,25 @@ public class DishTest {
      */
     @Test
     public void testGetServingsWorksAsExpected() {
-        Assert.fail("Not implemented");
+        Assert.assertEquals(DEFAULT_SHARED_DISH_SERVINGS, dish.getServings());
     }
 
     /**
      * Checks the error checking system throws an Exception on faulty input.
      *
      */
-    @Test
-    public void testSetServingsSetsThrowsExceptionForSmallServings() {
-        Assert.fail("Not implemented");
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetServingsThrowsExceptionWhenServingsIsZero() {
+        dish.setServings(0);
+    }
+
+    /**
+     * Can't have negative servings.
+     *
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetServingsThrowsExceptionWhenServingsIsNegative() {
+        dish.setServings(-1);
     }
 
     /**
@@ -96,7 +144,9 @@ public class DishTest {
      */
     @Test
     public void testSetServingsSetsServingsSize() {
-        Assert.fail("Not implemented");
+        int newServingsSize = DEFAULT_SHARED_DISH_SERVINGS * 3;
+        dish.setServings(newServingsSize);
+        Assert.assertEquals(newServingsSize, dish.getServings());
     }
 
     /**
@@ -105,7 +155,7 @@ public class DishTest {
      */
     @Test
     public void testGetImageRetrievesTheImageValue() {
-        Assert.fail("Not implemented");
+        Assert.assertEquals(DEFAULT_SHARED_DISH_IMAGE, dish.getImage());
     }
 
     /**
@@ -114,6 +164,9 @@ public class DishTest {
      */
     @Test
     public void testSetImageChangesImageValue() {
-        Assert.fail("Not implemented");
+        String newImage = "Some Image Value";
+        dish.setImage(newImage);
+
+        Assert.assertEquals(newImage, dish.getImage());
     }
 }
