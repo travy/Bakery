ae506d7a17315a14d2baa3f5761086a8ea33053e
Travis Anthony Torres <travis@travistorres.com>
2017-06-11 14:01:05 -0400

    tests:  Recipe Mock
    Constructed a Mock object for representing a Recipes dependencies.

--- app/src/test/java/com/travistorres/bakery/models/RecipeTest.java
+++ app/src/test/java/com/travistorres/bakery/models/RecipeTest.java
@@ -9,6 +9,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 /**
  * RecipeTest
  *
@@ -19,13 +21,36 @@ import org.junit.Test;
  */
 
 public class RecipeTest {
+    private Dish dish;
+    private Ingredient[] ingredients;
+    private Step[] steps;
+    private Recipe recipe;
+
     /**
      * Constructs an ingredient which can be used for all tests.
      *
      */
     @Before
     public void setUp() {
+        this.dish = mock(Dish.class);
 
+        Ingredient[] ingredients = new Ingredient[3];
+        ingredients[0] = mock(Ingredient.class);
+        ingredients[1] = mock(Ingredient.class);
+        ingredients[2] = mock(Ingredient.class);
+        this.ingredients = ingredients;
+
+        Step steps[] = new Step[3];
+        steps[0] = mock(Step.class);
+        steps[1] = mock(Step.class);
+        steps[2] = mock(Step.class);
+        this.steps = steps;
+
+        this.recipe = new Recipe(
+                this.dish,
+                this.ingredients,
+                this.steps
+        );
     }
 
     /**
@@ -34,7 +59,10 @@ public class RecipeTest {
      */
     @After
     public void tearDown() {
-
+        this.dish = null;
+        this.ingredients = null;
+        this.steps = null;
+        this.recipe = null;
     }
 
     @Test(expected = IllegalArgumentException.class)
