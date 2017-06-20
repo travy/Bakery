a164bc16afabc540082ba140900f30f4367c4028
Travis Anthony Torres <travis@travistorres.com>
2017-06-19 22:50:06 -0400

    chore:  Blank Activity
    Removed all unnecessary activities and one that is blank.

---
+++ app/src/main/java/com/travistorres/bakery/ExperimentalActivity.java
@@ -0,0 +1,13 @@
+package com.travistorres.bakery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExperimentalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_experimental);
+    }
+}
