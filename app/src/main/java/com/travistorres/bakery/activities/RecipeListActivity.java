62fb9e00d01d111510ee2960a8371fc27223df24
Travis Anthony Torres <travis@travistorres.com>
2017-06-21 19:07:26 -0400

    feat:  RecipeList Skeleton
    Created an activity which will be held responsible for displaying a list
    of all Recipes.

---
+++ app/src/main/java/com/travistorres/bakery/activities/RecipeListActivity.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) Travis Anthony Torres
+ */
+
+package com.travistorres.bakery.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.travistorres.bakery.R;
+
+/**
+ * RecipeListActivity
+ *
+ * Displays a list of Recipes which is loaded in from an external RESTful server.
+ *
+ * @author Travis Anthony Torres
+ * @version June 21, 2017
+ */
+
+public class RecipeListActivity extends AppCompatActivity {
+    /**
+     * Loads in all pertinent information for use in the list.  Also setsup the user interface so
+     * that they can be managed properly by the Activity.
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+    }
+}
---
+++ app/src/test/java/com/travistorres/bakery/activities/RecipeListActivityTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2017 Travis Anthony Torres
+ */
+
+package com.travistorres.bakery.activities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * RecipeListActivityTest
+ *
+ * Performs test operations on all methods used within the RecipeListActivity.
+ *
+ * @author Travis Anthony Torres
+ * @version June 21, 2017
+ */
+
+public class RecipeListActivityTest {
+    /**
+     * Test that when the RecipeListActivity completes, that a list of recipes is allocated memory.
+     *
+     */
+    @Test
+    public void testRecipeListActivityLoadsListOfRecipes() {
+        fail("Not implemented.");
+    }
+
+    /**
+     * Each of the view items in the layout resource should be mapped to some instance variable
+     * so that it can be controlled by the Activity.
+     *
+     */
+    @Test
+    public void testRecipeListActivityMapsAllInterfaceViewsToInstanceVariable() {
+        fail("Not implemented");
+    }
+}
