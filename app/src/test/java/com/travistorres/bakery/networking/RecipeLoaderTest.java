60c295212e215fcba21715ce87aa9cba9941168d
Travis Anthony Torres <travis@travistorres.com>
2017-07-18 21:11:13 -0400

    feat:  Loader
    Created a loader that will pull resources from an external server with
    Recipe information.

---
+++ app/src/test/java/com/travistorres/bakery/networking/RecipeLoaderTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2017 Travis Anthony Torres
+ */
+
+package com.travistorres.bakery.networking;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * RecipeLoaderTest
+ *
+ * Test all operations used for loading data.
+ *
+ * @author Travis Anthony Torres
+ * @version July 18, 2017
+ */
+
+public class RecipeLoaderTest {
+    /**
+     * Setup test environment before running each test.
+     *
+     */
+    @Before
+    public void setUp() {
+
+    }
+
+    /**
+     * Tear down the test environment after each test.
+     *
+     */
+    @After
+    public void tearDown() {
+
+    }
+
+    /**
+     * Test the getRetrofitObject method.
+     *
+     */
+    @Test
+    public void testGetRetrofitObject() {
+        fail("Not implemented.");
+    }
+
+    /**
+     * Test the load method.
+     *
+     */
+    @Test
+    public void testLoad() {
+        fail("Not implemented.");
+    }
+
+    /**
+     * Test the loadWithGsonConverter method.
+     *
+     */
+    @Test
+    public void testLoadWithGsonConverter() {
+        fail("Not implemented.");
+    }
+}
