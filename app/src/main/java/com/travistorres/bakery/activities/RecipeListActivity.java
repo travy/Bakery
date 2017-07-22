1b29b48be92e87428cb6868bf7caeaa8b95540e9
Travis Anthony Torres <travis@travistorres.com>
2017-07-22 12:07:43 -0400

    feat:  Views
    Created a main view for the RecipeListActivity containing a Recycler
    View and created the layout for each of the Recycler Views items.
    
    Removed unnecessary code testing

--- app/src/main/java/com/travistorres/bakery/activities/RecipeListActivity.java
+++ app/src/main/java/com/travistorres/bakery/activities/RecipeListActivity.java
@@ -22,8 +22,6 @@ import java.util.List;
  */
 
 public class RecipeListActivity extends AppCompatActivity {
-    private List<Recipe> recipes;
-
     /**
      * Loads in all pertinent information for use in the list.  Also setsup the user interface so
      * that they can be managed properly by the Activity.
@@ -34,34 +32,5 @@ public class RecipeListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
-    }
-
-    /**
-     * Sets a List of Recipes to display.
-     *
-     * @param recipes
-     *
-     * @return true if a List of Recipes was successfully saved and false otherwise.
-     */
-    public boolean setRecipes(List<Recipe> recipes) {
-        return false;
-    }
-
-    /**
-     * Retrieves a copy List of Recipes loaded.
-     *
-     * @return List of Recipe objects or null if none have been defined.
-     */
-    public List<Recipe> getRecipes() {
-        return null;
-    }
-
-    /**
-     * Retrieves a count of all recipes.
-     *
-     * @return Number of Recipes that has been loaded.
-     */
-    public long getNumberOfRecipes() {
-        return 0;
     }
 }
--- app/src/test/java/com/travistorres/bakery/activities/RecipeListActivityTest.java
+++ app/src/test/java/com/travistorres/bakery/activities/RecipeListActivityTest.java
@@ -4,13 +4,9 @@
 
 package com.travistorres.bakery.activities;
 
-import com.travistorres.bakery.models.Recipe;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -43,98 +39,5 @@ public class RecipeListActivityTest {
     @After
     public void tearDown() {
         activity = null;
-    }
-
-    /**
-     * Test that when the RecipeListActivity completes, that a list of recipes is allocated memory.
-     *
-     */
-    @Test
-    public void testRecipeListActivityLoadsListOfRecipes() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * Each of the view items in the layout resource should be mapped to some instance variable
-     * so that it can be controlled by the Activity.
-     *
-     */
-    @Test
-    public void testRecipeListActivityMapsAllInterfaceViewsToInstanceVariable() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * Old list of Recipes should be replaced by the newly loaded list.
-     *
-     */
-    @Test
-    public void testSetRecipeReplacesOldList() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * A value of false should be returned on bad input.
-     *
-     */
-    @Test
-    public void testSetRecipeReturnsFalseOnNullInput() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * A value of true should be returned on valid input.
-     *
-     */
-    @Test
-    public void testSetRecipeReturnsTrueWithValidResults() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * When <em>getRecipe()</em> is called, the <em>List</em> value returned should not have the
-     * same reference id.  This is to prevent manipulation of data from an external source.
-     *
-     */
-    @Test
-    public void testGetRecipeReturnedListIsNotSameObjectInMemory() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * The returned <em>List</em> should have all <em>Recipe</em> objects as in the
-     * <em>recipes</em> instance variable.
-     *
-     */
-    @Test
-    public void testGetRecipeRetrievesAnExactCopyOfRecipeList() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * <em>getRecipe()</em> should return a copy of the value just set by <em>setRecipe()</em>.
-     *
-     */
-    @Test
-    public void testGetRecipeRetrievesSameResultsAfterSetRecipeCall() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * When no list of Recipes is provided, the value returned should still be zero.
-     *
-     */
-    @Test
-    public void testGetNumberOfRecipesIsZeroWhenGetRecipesIsNull() {
-        fail("Not implemented.");
-    }
-
-    /**
-     * Method call should return the correct count of items.
-     *
-     */
-    @Test
-    public void testGetNumberOrRecipesReturnsCorrectValue() {
-        fail("Not implemented.");
     }
 }
