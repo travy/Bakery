bdb1970af180c7ff1bb7ab3cf7d7aaa0e7ca3f0d
Travis Anthony Torres <travis@travistorres.com>
2017-08-12 19:31:36 -0400

    refactor:  Recipe Activity
    Moved all RecyclerView classes to an external file.

--- app/src/main/java/com/travistorres/bakery/activities/RecipeActivity.java
+++ app/src/main/java/com/travistorres/bakery/activities/RecipeActivity.java
@@ -1,23 +1,16 @@
 package com.travistorres.bakery.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.travistorres.bakery.R;
 import com.travistorres.bakery.models.Recipe;
-import com.travistorres.bakery.models.Step;
 import com.travistorres.bakery.recyclerviews.adapters.RecipeIngredientAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.travistorres.bakery.recyclerviews.adapters.RecipeStepAdapter;
 
 public class RecipeActivity extends AppCompatActivity {
     private Recipe recipe;
@@ -25,70 +18,6 @@ public class RecipeActivity extends AppCompatActivity {
     private TextView recipeTitleTextView;
     private RecyclerView ingredientsRecyclerView;
     private RecyclerView stepsRecyclerView;
-
-    private class RecipeStepViewHolder extends RecyclerView.ViewHolder {
-        private TextView stepNumberTextView;
-        private TextView stepDescriptionTextView;
-
-        public RecipeStepViewHolder(View itemView) {
-            super(itemView);
-
-            stepNumberTextView = (TextView) itemView.findViewById(R.id.recipe_step_number);
-            stepDescriptionTextView = (TextView) itemView.findViewById(R.id.recipe_step_description);
-        }
-
-        public TextView getStepNumberTextView() {
-            return stepNumberTextView;
-        }
-
-        public TextView getStepDescriptionTextView() {
-            return stepDescriptionTextView;
-        }
-    }
-
-    private class RecipeStepAdapter extends RecyclerView.Adapter {
-        private List<Step> stepsList;
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            Context context = parent.getContext();
-            LayoutInflater inflater = LayoutInflater.from(context);
-            View view = inflater.inflate(
-                    R.layout.recipe_step,
-                    parent,
-                    false);
-
-            return new RecipeStepViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            Step step = stepsList.get(position++);
-            String stepNumber = Integer.toString(position);
-            String description = step.getShortDescription();
-
-            RecipeStepViewHolder view = (RecipeStepViewHolder) holder;
-            view.getStepNumberTextView().setText(stepNumber);
-            view.getStepDescriptionTextView().setText(description);
-        }
-
-        @Override
-        public int getItemCount() {
-            return stepsList == null ? 0 : stepsList.size();
-        }
-
-        public List<Step> setSteps(Step[] steps) {
-            stepsList = new ArrayList<Step>();
-
-            for(Step step : steps) {
-                stepsList.add(step);
-            }
-
-            notifyDataSetChanged();
-
-            return stepsList;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/travistorres/bakery/recyclerviews/viewholders/RecipeStepViewHolder.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2017 Travis Anthony Torres
+ */
+
+package com.travistorres.bakery.recyclerviews.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.travistorres.bakery.R;
+
+/**
+ * RecipeStepViewHolder
+ *
+ * Provides an interface for specifying the contents of a Steps tabs user interface.
+ *
+ * @author Travis Anthony Torres
+ * @version August 12, 2017
+ */
+
+public class RecipeStepViewHolder extends RecyclerView.ViewHolder {
+    private TextView stepNumberTextView;
+    private TextView stepDescriptionTextView;
+
+    /**
+     * Retrieves all interface objects so that they can be pragmatically manipulated.
+     *
+     * @param itemView
+     */
+    public RecipeStepViewHolder(View itemView) {
+        super(itemView);
+
+        stepNumberTextView = (TextView) itemView.findViewById(R.id.recipe_step_number);
+        stepDescriptionTextView = (TextView) itemView.findViewById(R.id.recipe_step_description);
+    }
+
+    /**
+     * Acquires a reference to the step counter.
+     *
+     * @return Specifies the step number in the Recipe sequence
+     */
+    public TextView getStepNumberTextView() {
+        return stepNumberTextView;
+    }
+
+    /**
+     * Acquires the field responsible for displaying the description of the step.
+     *
+     * @return Specifies the description for the step
+     */
+    public TextView getStepDescriptionTextView() {
+        return stepDescriptionTextView;
+    }
+}
