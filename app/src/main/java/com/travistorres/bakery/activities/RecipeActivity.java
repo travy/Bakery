3e392dc198bd7d79eeed46100706d0930c58cd6d
Travis Anthony Torres <travis@travistorres.com>
2017-08-12 15:32:16 -0400

    refactor:  Ingredients Setup
    Cleaned up logic for Ingredients Recycler View

--- app/src/main/java/com/travistorres/bakery/activities/RecipeActivity.java
+++ app/src/main/java/com/travistorres/bakery/activities/RecipeActivity.java
@@ -1,24 +1,15 @@
 package com.travistorres.bakery.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.travistorres.bakery.R;
-import com.travistorres.bakery.models.Ingredient;
 import com.travistorres.bakery.models.Recipe;
 import com.travistorres.bakery.recyclerviews.adapters.RecipeIngredientAdapter;
-import com.travistorres.bakery.recyclerviews.viewholders.RecipeIngredientViewHolder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class RecipeActivity extends AppCompatActivity {
     private Recipe recipe;
@@ -37,10 +28,16 @@ public class RecipeActivity extends AppCompatActivity {
         recipeTitleTextView = (TextView) findViewById(R.id.recipe_name);
         recipeTitleTextView.setText(recipe.getName());
 
+        setupIngredientsRecyclerView();
+    }
+
+    private void setupIngredientsRecyclerView() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         RecipeIngredientAdapter adapter = new RecipeIngredientAdapter();
+
         ingredientsRecyclerView = (RecyclerView) findViewById(R.id.recipe_ingredients_rv);
+        ingredientsRecyclerView.setLayoutManager(layoutManager);
         ingredientsRecyclerView.setAdapter(adapter);
-        ingredientsRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         adapter.setIngredients(recipe.getIngredients());
     }
