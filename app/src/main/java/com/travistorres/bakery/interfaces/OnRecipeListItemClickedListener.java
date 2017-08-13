38b0578334b2d98646d17f878d3d728e31eb98a1
Travis Anthony Torres <travis@travistorres.com>
2017-08-13 00:32:08 -0400

    docs:  OnRecipeListItemClickedListener
    documented interface

--- app/src/main/java/com/travistorres/bakery/interfaces/OnRecipeListItemClickedListener.java
+++ app/src/main/java/com/travistorres/bakery/interfaces/OnRecipeListItemClickedListener.java
@@ -1,9 +1,25 @@
+/**
+ * Copyright (c) 2017 Travis Anthony Torres
+ */
+
 package com.travistorres.bakery.interfaces;
 
 import com.travistorres.bakery.models.Recipe;
 
-//  TODO:  document interface
+/**
+ * OnRecipeListItemClickedListener
+ *
+ * Specifies what to do when a Recipe object has been clicked.
+ *
+ * @author Travis Anthony Torres
+ * @version August 13, 2017
+ */
 
 public interface OnRecipeListItemClickedListener {
+    /**
+     * Provides a handle to a Recipe that has been clicked.
+     *
+     * @param recipe
+     */
     void onClick(Recipe recipe);
 }
