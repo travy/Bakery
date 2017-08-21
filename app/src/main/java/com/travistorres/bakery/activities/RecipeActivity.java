3fe23e1719cb484bf7d0376681b8634a60043f38
Travis Anthony Torres <travis@travistorres.com>
2017-08-20 20:23:15 -0400

    feat:  Select Step
    Updates the contents of the Step Detail view when a Step is selected
    from the list.

--- app/src/main/java/com/travistorres/bakery/activities/RecipeActivity.java
+++ app/src/main/java/com/travistorres/bakery/activities/RecipeActivity.java
@@ -2,11 +2,13 @@ package com.travistorres.bakery.activities;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.travistorres.bakery.R;
+import com.travistorres.bakery.fragments.RecipeStepsItemFragment;
 import com.travistorres.bakery.fragments.RecipeStepsListFragment;
 import com.travistorres.bakery.interfaces.RecipeMasterDetailFlowInterface;
 import com.travistorres.bakery.models.Recipe;
@@ -15,7 +17,8 @@ import com.travistorres.bakery.models.Step;
 //  TODO- document
 public class RecipeActivity extends AppCompatActivity implements RecipeMasterDetailFlowInterface {
     private Recipe recipe;
-    private RecipeStepsListFragment fragment;
+    private RecipeStepsListFragment stepsListFragment;
+    private RecipeStepsItemFragment stepDetailFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +28,17 @@ public class RecipeActivity extends AppCompatActivity implements RecipeMasterDet
         Intent intent = getIntent();
         recipe = intent.getParcelableExtra(getString(R.string.extra_key_for_recipe));
 
-        Fragment fragment = new RecipeStepsListFragment();
-        getSupportFragmentManager()
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        stepsListFragment = new RecipeStepsListFragment();
+        fragmentManager
                 .beginTransaction()
-                .add(R.id.recipe_steps_list_fragment, fragment)
+                .add(R.id.recipe_steps_list_fragment, stepsListFragment)
                 .commit();
+
+        stepDetailFragment = (RecipeStepsItemFragment) fragmentManager
+                        .findFragmentById(R.id.fragment_recipe_step_item);
     }
 
     @Override
@@ -39,6 +48,6 @@ public class RecipeActivity extends AppCompatActivity implements RecipeMasterDet
 
     @Override
     public void onSelectedStep(Step step) {
-        Toast.makeText(this, step.getDescription(), Toast.LENGTH_SHORT).show();
+        stepDetailFragment.setStep(step);
     }
 }
--- app/src/main/java/com/travistorres/bakery/fragments/RecipeStepsItemFragment.java
+++ app/src/main/java/com/travistorres/bakery/fragments/RecipeStepsItemFragment.java
@@ -6,15 +6,64 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.travistorres.bakery.R;
+import com.travistorres.bakery.models.Step;
 
 public class RecipeStepsItemFragment extends Fragment {
+    private Button nextStepButton;
+    private Button previousStepButton;
+    private TextView descriptionTextView;
+    private TextView shortDescriptionTextView;
+
+    private Step step;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recipe_step_instruction_list_item, container, false);
+        return inflater.inflate(R.layout.fragment_recipe_step_instruction_list_item, container, false);
+    }
 
-        return view;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        View view = getView();
+        nextStepButton = (Button) view.findViewById(R.id.next_step_button);
+        previousStepButton = (Button) view.findViewById(R.id.previous_step_button);
+        descriptionTextView = (TextView) view.findViewById(R.id.step_description);
+        shortDescriptionTextView = (TextView) view.findViewById(R.id.step_short_description);
+
+        setStep(null);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public void setStep(@Nullable Step newStep) {
+        step = newStep;
+
+        updateStepInformation();
+    }
+
+    private void updateStepInformation() {
+        boolean isStepSpecified = step != null;
+        int viewVisibility = isStepSpecified ?
+                View.VISIBLE :
+                View.INVISIBLE;
+
+        nextStepButton.setVisibility(viewVisibility);
+        previousStepButton.setVisibility(viewVisibility);
+        descriptionTextView.setVisibility(viewVisibility);
+        shortDescriptionTextView.setVisibility(viewVisibility);
+
+        if (isStepSpecified) {
+            descriptionTextView.setText(step.getDescription());
+            shortDescriptionTextView.setText(step.getShortDescription());
+        }
     }
 }
